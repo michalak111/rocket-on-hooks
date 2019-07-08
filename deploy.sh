92eb51c96619890716665fefb5ac1cfc586c0876
Jan Michalak <michalak111@gmail.com>
2019-07-08 22:33:29 +0200

    [wip] circleci - setup docker

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 ssh testsite@vps704954.ovh.net -o StrictHostKeyChecking=no <<EOF
   cd rocket-on-hooks
-  git checkout $1
+#  git checkout $1
   git pull
   docker-compose -f docker-compose.yml -p $1 stop
   docker-compose -f docker-compose.yml -p $1 up -e BRANCH=$1 --build -d
