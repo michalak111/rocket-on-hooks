6bce7ef3d7441d5e65d87cba4bb824be261c4887
michalak111 <michalak111@gmail.com>
2019-07-09 10:40:19 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ssh testsite@vps704954.ovh.net -o StrictHostKeyChecking=no <<EOF
+ssh rocket-on-hooks@vps704954.ovh.net -o StrictHostKeyChecking=no <<EOF
   git checkout $1
   git pull
   docker-compose -f docker-compose.yml -p $1 stop
